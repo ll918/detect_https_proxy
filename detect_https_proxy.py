4c08c03785b9f1c1bcbad0afd2c84403b88ce07f
bouc <bouc@Justine.local>
2016-08-08 16:29:17 -0400

    Initial commit

---
+++ detect_https_proxy.py
@@ -0,0 +1,3 @@
+#!/usr/local/bin/env python3
+# Detecting https proxy by getting the ssl certificate sha1 fingerprint with grc.com verified fingerprints. if the digests are diferent it might be explained by the existence of an https proxy.
+
