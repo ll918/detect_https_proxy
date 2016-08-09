098f40a5d648c53acfd357ea536341e18c0c2e7d
ll918 <ll918@users.noreply.github.com>
2016-08-09 17:02:18 -0400

    Added a dict of common websites and their verified fingerprints. Added code to compare fingerprints against verified fingerprints. This is now a full implementation of the concepts described here: https://www.grc.com/fingerprints.htm General code cleanup and reformat.

--- detect_https_proxy.py
+++ detect_https_proxy.py
@@ -1,14 +1,22 @@
 #!/usr/local/bin/env python3
-# Detecting https proxy by getting the security certificate sha1 fingerprint with grc.com list of verified fingerprints.
-# If the digests are different it might be explained by the existence of an https proxy.
+# Detecting https proxy by comparing the sha1 fingerprint of a security certificate for a web site with a verified
+# fingerprint. If the digests are different it might be explained by the existence of an https proxy.
+# Verified fingerprints from https://www.grc.com/fingerprints.htm
 
 import hashlib
 import ssl
 
-host = "www.grc.com"
-host2 = "www.facebook.com"
-host3 = "www.paypal.com"
 https_port = 443
+verified_fingerprint = {"www.grc.com": "3fc3245c36b389b175ca20c01fc0f1494b7473e6",
+                        "www.facebook.com": "a04eafb348c26b15a8c1aa87a333caa3cdeec9c9",
+                        "www.paypal.com": "b9c971668c4e377b82bdee9b07f9c191b6ee59de",
+                        "www.wikipedia.org": "87f5babbd897c579b66af52fd8638b99bd1ce826",
+                        "twitter.com": "235a79b3270d790505e0bea2cf5c149f9038821b",
+                        "www.blogger.com": "24c18787dbf8fd23b77077cd8b266adc174ff2a5",
+                        "www.linkedin.com": "a33bb54cbc57513f94a3c288a64a02c5f68d4057",
+                        "www.yahoo.com": "413072f803ce961210e9a45d10da14b0d2d48532",
+                        "wordpress.com": "1fe2d64111fca8d71b234ffceca86d80de17d94a",
+                        "www.wordpress.com": "54e089df28538300105dd43764fde7d0f5ed5bc0"}
 
 
 def get_cert_fingerprint(certificate):
@@ -19,8 +27,11 @@ def get_cert_fingerprint(certificate):
     return digest
 
 
-certificate = ssl.get_server_certificate((host, https_port))
-fingerprint = get_cert_fingerprint(certificate)
-print(fingerprint)
+for k, v in verified_fingerprint.items():
+    certificate = ssl.get_server_certificate((k, https_port))
+    fingerprint = get_cert_fingerprint(certificate)
 
-# todo compare fingerprint with verified fingerprint
+    if fingerprint == v:
+        print(k + ": fingerprint ok")
+    else:
+        print(k + ": fingerprint is " + fingerprint + " instead of " + v)
