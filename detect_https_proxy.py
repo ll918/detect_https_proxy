e6337dfae3877855387d51b03a9aa3a387d83b04
ll918 <gauvinjp@gmail.com>
2016-08-08 23:26:07 -0400

    Initial commit. Security certificate fingerprinting implemented.

--- detect_https_proxy.py
+++ detect_https_proxy.py
@@ -1,3 +1,26 @@
 #!/usr/local/bin/env python3
-# Detecting https proxy by getting the ssl certificate sha1 fingerprint with grc.com verified fingerprints. if the digests are diferent it might be explained by the existence of an https proxy.
+# Detecting https proxy by getting the security certificate sha1 fingerprint with grc.com list of verified fingerprints.
+# If the digests are different it might be explained by the existence of an https proxy.
 
+import hashlib
+import ssl
+
+host = "www.grc.com"
+host2 = "www.facebook.com"
+host3 = "www.paypal.com"
+https_port = 443
+
+
+def get_cert_fingerprint(certificate):
+    h = hashlib.sha1()
+    der_cert = ssl.PEM_cert_to_DER_cert(certificate)
+    h.update(der_cert)
+    digest = h.hexdigest()
+    return digest
+
+
+certificate = ssl.get_server_certificate((host, https_port))
+fingerprint = get_cert_fingerprint(certificate)
+print(fingerprint)
+
+# todo compare fingerprint with verified fingerprint
