8866611647fd1d79671d97734286a5958b29f45b
ll918 <ll918@users.noreply.github.com>
2016-12-21 19:04:04 -0500

    Added exception handling.

--- detect_https_proxy.py
+++ detect_https_proxy.py
@@ -7,6 +7,7 @@ Verified fingerprints from https://www.grc.com/fingerprints.htm
 """
 import hashlib
 import ssl
+import sys
 from collections import OrderedDict
 
 https_port = 443
@@ -16,7 +17,6 @@ verified_fingerprint = {
     'www.paypal.com': 'b9c971668c4e377b82bdee9b07f9c191b6ee59de',
     'www.wikipedia.org': '586684ef773ea0b85f233873cb4610e8d0e08cb3',
     'twitter.com': '235a79b3270d790505e0bea2cf5c149f9038821b',
-    'www.blogger.com': '71d8bc65f7e721dd2daca3c3288deb7bf5621019',
     'www.linkedin.com': '3a6039e8cee4fb5887b85397898f049820bfe391',
     'www.yahoo.com': '413072f803ce961210e9a45d10da14b0d2d48532',
     'wordpress.com': '1fe2d64111fca8d71b234ffceca86d80de17d94a',
@@ -38,22 +38,28 @@ def get_cert_fingerprint(certificate):
 
 def get_certificate(site):
     """get a web site and return a secure certificate in DER format"""
-    pem_cert = ssl.get_server_certificate((site, https_port))
-    der_cert = ssl.PEM_cert_to_DER_cert(pem_cert)
+    der_cert = b''
+    try:
+        pem_cert = ssl.get_server_certificate((site, https_port))
+        der_cert = ssl.PEM_cert_to_DER_cert(pem_cert)
+    except:
+        print("Unexpected error:", sys.exc_info()[0],
+              'retrieving certificate from', site)
+
     return der_cert
 
 
 wrong_fingerprint = 0
 for website, ver_fprint in ver_fing_sorted.items():
     certificate = get_certificate(website)
-    fingerprint = get_cert_fingerprint(certificate)
-
-    if fingerprint == ver_fprint:
-        print(website, ': fingerprint ok')
-    else:
-        wrong_fingerprint += 1
-        print(website, ': fingerprint is', fingerprint, 'instead of',
-              ver_fprint)
+    if certificate != b'':
+        fingerprint = get_cert_fingerprint(certificate)
+        if fingerprint == ver_fprint:
+            print(website, ': fingerprint ok')
+        else:
+            wrong_fingerprint += 1
+            print(website, ': fingerprint is', fingerprint, 'instead of',
+                  ver_fprint)
 
 if wrong_fingerprint > 0:
     print()
@@ -65,5 +71,4 @@ if wrong_fingerprint > 0:
         'Check that your verified fingerprints are accurate and up to date then try again.')
     print('For more info: https://www.grc.com/fingerprints.htm')
 else:
-    print()
-    print('No HTTPS proxy detected.')
+    pass
